98a595de56725208698f8bc161e68b11ca0be996
NoobHackLol <91553292+NoobHackLol@users.noreply.github.com>
2021-10-12 15:36:15 -0400

    LOLS

---
+++ NoobHack/src/main/java/me/awsomemanpop1/noobhack/util/Refrence.java
@@ -0,0 +1,12 @@
+package me.awsomemanpop1.noobhack.util;
+
+public class Refrence {
+	
+	public static final String MOD_ID = "nha";
+	public static final String NAME = "NoobHack";
+	public static final String VERSION = "0.0.1";
+	public static final String ACCEPTED__VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "me.awsomemanpop1.noobhack.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "me.awsomemanpop1.noobhack.CommonProxy";
+
+}
