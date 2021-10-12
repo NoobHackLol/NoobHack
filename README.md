ee4bcbb4befb10731b35f86e7df2b7685aad4045
NoobHackLol <91553292+NoobHackLol@users.noreply.github.com>
2021-10-12 15:17:19 -0400

    lol
    ok

--- README.md
+++
@@ -1,5 +0,0 @@
-# NoobHack
-A minecraft java 1.12.2 hacked client.
-
-
-Put the jar file in your mods folder.
---
+++ src/main/java/me/awsomemanpop1/noobhack/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package me.awsomemanpop1.noobhack.proxy;
+
+public class ClientProxy extends CommonProxy{
+
+}
